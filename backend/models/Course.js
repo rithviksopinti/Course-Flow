a8474d4d95a4b2f0a3ef74056e781738547951f2
Rithvik Sopinti <rithvikpersonal0.0@gmail.com>
2026-04-19 10:56:14 +0530

    Delete backend directory

--- backend/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const courseSchema = new mongoose.Schema({
-  title: String,
-  video_url: String,
-  description: String
-});
-
-module.exports = mongoose.model("Course", courseSchema);
